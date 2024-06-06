ca4ff109d4b48a6d7c4bd707810eebbb0b9131fa
maddusahot <ahmaddoust@gmail.com>
2024-06-05 20:39:00 -0400

    updated files

--- concurrency/v1/check_website.go
+++ concurrency/v1/check_website.go
@@ -1,6 +1,8 @@
 package concurrency
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // CheckWebsite returns true if the URL returns a 200 status code, false otherwise.
 func CheckWebsite(url string) bool {
